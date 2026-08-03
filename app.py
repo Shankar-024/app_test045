273ad3ee3e4e5605ec413cb18700c3100241e110
Shankar-024 <145898961+Shankar-024@users.noreply.github.com>
2026-08-03 10:53:57 +0530

    Add age input and voting eligibility check

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ st.title("my first streamlit app")
 name=st.text_input("enter your name")
 if st.button("Submit"):
   st.write(f"Hello,{name}")
+
+
+age=st.int_input("Enter ur age")
+if st.button("submit"):
+  st.write("Eligible to vote")
+else:
+  st.write("Not eligible to vote")
